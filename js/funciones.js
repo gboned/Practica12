4d47e1fb4e0ff6f06930391116b37210b4807b77
gboned <gboned@cifpfbmoll.eu>
2020-06-04 03:35:58 +0200

    Añadir variable global con la lista de letras

--- js/funciones.js
+++ js/funciones.js
@@ -3,3 +3,7 @@
 /*        DETECTOR DE DNI FALSOS         */
 /*                                       */           
 /*****************************************/
+
+// Creo un array con la lista de posibles letras que puede contener un DNI.
+listaLetras = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", 
+                "V", "H", "L", "C", "K", "E", "T"];
