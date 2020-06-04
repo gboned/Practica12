2ce163a950eabcfdd2fbca0b5072543179bffe6a
gboned <gboned@cifpfbmoll.eu>
2020-06-04 03:38:27 +0200

    Crear función que obtiene nombre introducido

--- js/funciones.js
+++ js/funciones.js
@@ -7,3 +7,10 @@
 // Creo un array con la lista de posibles letras que puede contener un DNI.
 listaLetras = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", 
                 "V", "H", "L", "C", "K", "E", "T"];
+
+// Creo una función que recupera el nombre introducido.
+function obtenerNombre() {
+    // Añado variable que guarda el valor introducido en el primer input.
+    var nombre = $("#nombre").val();
+    return nombre.toUpperCase();
+}
